c70ea3fe7689b631280800cf8b41bbf9fffe41b3
Angie Molina <angiesayagom27@outlook.es>
2024-12-21 21:38:52 -0500

    se agrega crud novedades

--- src/routers/apiV1Router.js
+++ src/routers/apiV1Router.js
@@ -2,6 +2,8 @@ import { Router } from "express"
 import { SeasonService } from "../services/season.js"
 import userRouters from "./users.js";
 import saleRouters from "./saleRouter.js";
+import newsRouters from "./new.js";
+
 const apiV1Router = Router()
 
 const APIV1 = '/api/v1'
@@ -9,5 +11,6 @@ const APIV1 = '/api/v1'
 apiV1Router.use(APIV1, userRouters);
 apiV1Router.use(APIV1, saleRouters)//PUBLICAR API
 apiV1Router.use(APIV1, createSeasonRouter({ seasonService: SeasonService }));
+apiV1Router.use(APIV1, newsRouters);
 
 export default apiV1Router
