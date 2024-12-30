ed520cfa0e51437146b20e385062e687c1c9fabe
anfecv <afchv09@gmail.com>
2024-12-30 14:19:29 -0500

    Validation of crop information

--- src/routers/crop.js
+++ src/routers/crop.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { getCrops, getCrop, postCrop, putCrop, deleteCrop} from "../controllers/crop.js"; 
-
+import validateID from "../middlewares/validateID.js";
+import { verifyCrop } from "../middlewares/validateCrop.js";
 
 const cropRouters = Router()
 
 cropRouters.get('/crop',getCrops)
-cropRouters.get('/crop/:id',getCrop)
-cropRouters.post('/crop',postCrop)
-cropRouters.put('/crop/:id',putCrop)
-cropRouters.delete('/crop/:id',deleteCrop)
+cropRouters.get('/crop/:id',validateID,getCrop)
+cropRouters.post('/crop',verifyCrop,postCrop)
+cropRouters.put('/crop/:id',validateID,verifyCrop,putCrop)
+cropRouters.delete('/crop/:id',validateID,deleteCrop)
 
 export default cropRouters;
