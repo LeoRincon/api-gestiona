cab3595e03c4888c16a46ddac5ab3042373d5197
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2024-12-30 19:26:35 -0500

    Add authentication routes for login and logout functionality

--- src/routers/apiV1Router.js
+++ src/routers/apiV1Router.js
@@ -1,26 +1,28 @@
-import { Router } from 'express';
-import suppliesRouters from "./supplies.js";
-import expensesRouters from "./expenses.js";
-import activityRouter from './activity.js';
-import cropRouters from './crop.js';
-import newsRouters from './new.js';
-import saleRouter from './sales.js';
-import seasonRouter from './seasons.js';
-import userRouters from './users.js';
-import projectRouters from './project.js';
+import { Router } from 'express'
+import suppliesRouters from './supplies.js'
+import expensesRouters from './expenses.js'
+import activityRouter from './activity.js'
+import cropRouters from './crop.js'
+import newsRouters from './new.js'
+import saleRouter from './sales.js'
+import seasonRouter from './seasons.js'
+import userRouters from './users.js'
+import projectRouters from './project.js'
+import authRouters from './authRouters.js'
 
-const apiV1Router = Router();
+const apiV1Router = Router()
 
-const APIV1 = '/api/v1';
+const APIV1 = '/api/v1'
 
-apiV1Router.use(APIV1, activityRouter);
-apiV1Router.use(APIV1, cropRouters);
-apiV1Router.use(APIV1, newsRouters);
-apiV1Router.use(APIV1, saleRouter);
-apiV1Router.use(APIV1, seasonRouter);
-apiV1Router.use(APIV1, userRouters);
-apiV1Router.use(`${APIV1}/supplies`, suppliesRouters);
-apiV1Router.use(`${APIV1}/expenses`, expensesRouters);
-apiV1Router.use(APIV1, projectRouters);
+apiV1Router.use(APIV1, authRouters)
+apiV1Router.use(APIV1, activityRouter)
+apiV1Router.use(APIV1, cropRouters)
+apiV1Router.use(APIV1, newsRouters)
+apiV1Router.use(APIV1, saleRouter)
+apiV1Router.use(APIV1, seasonRouter)
+apiV1Router.use(APIV1, userRouters)
+apiV1Router.use(`${APIV1}/supplies`, suppliesRouters)
+apiV1Router.use(`${APIV1}/expenses`, expensesRouters)
+apiV1Router.use(APIV1, projectRouters)
 
-export default apiV1Router;
+export default apiV1Router
