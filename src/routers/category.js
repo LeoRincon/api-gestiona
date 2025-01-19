cdb506f38c704c6d8090557dceab0bc21d105f7f
Leonardo Rincon <jleobotache@gmail.com>
2025-01-19 12:00:51 -0500

    Add category validation middleware and schema for category management

--- src/routers/category.js
+++ src/routers/category.js
@@ -6,13 +6,20 @@ import {
  deleteCategory,
  updatedCategory,
 } from '../controllers/category.js';
+import { verifyCategory } from '../middlewares/validateCategory.js';
+import validateID from '../middlewares/validateID.js';
 
 const categoryRouters = Router();
 
 categoryRouters.get('/categories', getCategories);
-categoryRouters.get('/categories/:id', getCategoryByID);
-categoryRouters.post('/categories', createCategory);
-categoryRouters.put('/categories/:id', updatedCategory);
-categoryRouters.delete('/categories/:id', deleteCategory);
+categoryRouters.get('/categories/:id', validateID, getCategoryByID);
+categoryRouters.post('/categories', verifyCategory, createCategory);
+categoryRouters.put(
+ '/categories/:id',
+ validateID,
+ verifyCategory,
+ updatedCategory
+);
+categoryRouters.delete('/categories/:id', validateID, deleteCategory);
 
 export default categoryRouters;
