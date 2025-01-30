19f068a42573c6f115a05dd7fdac6c794a8a25a5
Darwin Rojas V. <dwnrojasvalencia@outlook.com>
2025-01-30 00:06:06 -0500

    Add endpoint to fetch sales by season ID and refactor sale controller methods

--- src/routers/sales.js
+++ src/routers/sales.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllsale ,getSale , postSale , deleteSale, putSale} from "../controllers/saleController.js";
+import { getAllsale ,getSale , postSale , deleteSale, putSale, getAllSalesBySeasonId} from "../controllers/saleController.js";
 import validateID from '../middlewares/validateID.js'
 import { verifySale} from '../middlewares/validateSale.js'
 
@@ -10,6 +10,7 @@ saleRouter.get('/sales/:id',validateID,getSale)
 saleRouter.post('/sales',verifySale,postSale)
 saleRouter.put('/sales/:id',validateID,verifySale,putSale)
 saleRouter.delete('/sales/:id',validateID,deleteSale)
+saleRouter.get('/seasons/:id/sales', getAllSalesBySeasonId)
 
 
 
